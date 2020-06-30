5b10cd9109244d9c3fccdca8656770cb4f9ee4a8
Alex Schloegl <alex@try2catch.com>
2020-06-30 22:08:25 +0200

    Bugfix

--- src/Db/Product/Entity.php
+++ src/Db/Product/Entity.php
@@ -83,7 +83,7 @@ class Entity
 	 * @ORM\ManyToMany(targetEntity="Ecommerce\Db\Product\Attribute\Value\Entity")
 	 * @ORM\JoinTable(name="ecommerce_products__attribute_values",
 	 *      joinColumns={@ORM\JoinColumn(name="productId", referencedColumnName="id", onDelete="CASCADE")},
-	 *      inverseJoinColumns={@ORM\JoinColumn(name="productAttributeValueId", referencedColumnName="id")
+	 *      inverseJoinColumns={@ORM\JoinColumn(name="productAttributeValueId", referencedColumnName="id")}
 	 *      )
 	 */
 	private $attributeValues;
